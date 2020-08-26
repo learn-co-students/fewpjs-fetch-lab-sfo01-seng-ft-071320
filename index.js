c502e40e45c1b6a33abd6efff6b1bd5cdcccc0a4
Taci Shlosberg <tshlosberg@gmail.com>
2020-08-25 22:25:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-function fetchBooks() {
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
 
+})
+console.log("after")
+
+// function fetchBooks() {
+//   const promise = fetch('https://anapioficeandfire.com/api/books');
+
+//   const pr2 = promise.then(function processResult(response) {
+//     return response.json();
+//   });
+  
+//   pr2.then(function processJson(json) {
+//     renderBooks(json);
+//   })
+// }
+
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      renderBooks(json);
+    });
 }
+
+//lambda function
+// function fetchBooks() {
+//   return fetch('https://anapioficeandfire.com/api/books')
+//         .then(response => response.json())
+//         .then(json => renderBooks(json))
+// }
 
 function renderBooks(books) {
   const main = document.querySelector('main')
@@ -11,6 +42,5 @@ function renderBooks(books) {
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+
+
